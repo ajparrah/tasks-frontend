c6750e4c3c58a3aaf0598e2352daea54cbdb15fa
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-03-26 19:21:03 -0400

    feat: Show modal o add a new task

--- src/components/Tasks/AddTaskItem.tsx
+++ src/components/Tasks/AddTaskItem.tsx
@@ -1,12 +1,23 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Modal from '../Modal/Modal';
 
 interface IAddTaskItemProps {}
 
 const AddTaskItem: FC<IAddTaskItemProps> = () => {
+  const [showModalToAddTask, setShowModalToAddTask] = useState(false);
   return (
-    <div className="w-full bg-main border border-stroke flex justify-center items-center py-8 cursor-pointer hover:bg-gray-50">
-      <span className="text-center font-bold text-6xl">+</span>
-    </div>
+    <>
+      <div
+        className="w-full bg-main border border-stroke flex justify-center items-center py-8 cursor-pointer hover:bg-gray-50"
+        onClick={() => setShowModalToAddTask(true)}>
+        <span className="text-center font-bold text-6xl">+</span>
+      </div>
+      <Modal
+        open={showModalToAddTask}
+        onClose={() => setShowModalToAddTask(false)}>
+        <h1>Agregar nueva tarea</h1>
+      </Modal>
+    </>
   );
 };
 
