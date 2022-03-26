83330d83e846a8392c2615f44109226a187f5afb
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-03-26 13:58:59 -0400

    feat: Add date and format it

--- src/sections/Dashboard/Header.tsx
+++ src/sections/Dashboard/Header.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
+import { format } from 'date-fns';
 
 const Header: FC = () => {
   return (
     <div className="w-full flex justify-between items-center mb-4">
       <h1 className="font-bold">Cosas por hacer</h1>
-      <h3>Hoy: 14/08/2019</h3>
+      <h3>Hoy: {format(new Date(), 'dd/MM/yyyy')}</h3>
     </div>
   );
 };
